dd83b54f7cd8c4fccbdd5fb10af6c62771e4fa5a
luis juarez <contactoluisjuarez@gmail.com>
2025-02-26 10:09:12 -0400

    packet views and components

--- src/main/java/com/luisjuarez/sistemavu/SistemaVU.java
+++ src/main/java/com/luisjuarez/sistemavu/SistemaVU.java
@@ -1,8 +1,12 @@
 package com.luisjuarez.sistemavu;
 
+import com.luisjuarez.sistemavu.view.SistemaPrincipal;
+
+
 public class SistemaVU {
 
     public static void main(String[] args) {
-       
+        SistemaPrincipal sp = new SistemaPrincipal();
+        sp.setVisible(true);
     }
 }
